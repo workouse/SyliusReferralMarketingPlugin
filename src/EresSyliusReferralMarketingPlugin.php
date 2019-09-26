d825cec8afe106350038578909a9f5471b12f4fc
Ömer Büyükçelik <omerbuyukcelik@gmail.com>
2019-09-26 16:06:07 +0300

    namespace changed

--- src/EresSyliusReferralMarketingPlugin.php
+++ src/EresSyliusReferralMarketingPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Eres\SyliusReferralMarketingPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class EresSyliusReferralMarketingPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
