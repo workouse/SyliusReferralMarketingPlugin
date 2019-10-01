1b1ffa2ac940d7636484c90b742961f284411c4f
Ömer Büyükçelik <omerbuyukcelik@gmail.com>
2019-10-01 12:31:41 +0300

    updated: PromotionCoupon

--- src/Entity/PromotionCouponTrait.php
+++ src/Entity/PromotionCouponTrait.php
@@ -3,18 +3,11 @@
 
 namespace Eres\SyliusReferralMarketingPlugin\Entity;
 
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\JoinColumn;
-use Sylius\Component\Core\Model\PromotionCoupon as BasePromotionCoupon;
 use Sylius\Component\Customer\Model\Customer;
 
-/**
- * @Entity
- * @Table(name="sylius_promotion_coupon")
- */
-class PromotionCoupon extends BasePromotionCoupon
+trait PromotionCouponTrait
 {
     /**
      * @ManyToOne("Sylius\Component\Customer\Model\CustomerInterface")
